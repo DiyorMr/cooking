4d9953800cb5b3a294a6a24ee3d3d0f204480e75
DiyorMr <dmuxiddinov594@gmail.com>
2025-08-30 12:18:30 +0500

    next link

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,34 @@
 import { Facebook, Instagram, Twitter } from '@deemlol/next-icons'
 import React from 'react'
+import Link from 'next/link'
 
 const HomePage = () => {
   return (
     <>
+
       <div className='mx-auto my-5 w-full max-w-7xl'>
         <div className='flex flex-col items-center justify-center gap-5 md:flex-row md:justify-between'>
           <div>
             <img src="/img/logo.svg" alt="" />
           </div>
           <div className='flex flex-col items-center gap-2 md:flex-row md:gap-5'>
-            <ul className='flex flex-col items-center gap-2 md:flex-row md:gap-5 text-black'>
-              <li>Home</li>
-              <li>Recipes</li>
-              <li>Blog</li>
-              <li>Contact</li>
-              <li>About us</li>
+            <ul className='flex flex-col items-center gap-2 md:flex-row md:gap-5 text-black font-medium text-base'>
+              <Link href="/dashboard" replace>
+                Dashboard
+              </Link>
+              <Link href="/recipes" replace>
+                Recipes
+              </Link>
+              <Link href="/blog" replace>
+                Blog
+              </Link>
+              <Link href="/contact" replace>
+                Contact
+              </Link>
+              <Link href="/aboutus" replace>
+                About us
+              </Link>
+
             </ul>
           </div>
           <div className='flex items-center gap-5'>
@@ -27,13 +40,28 @@ const HomePage = () => {
       </div>
       <div>
         <div className='flex flex-col items-center justify-center gap-5 md:flex-row md:justify-between mx-auto my-5 w-full max-w-7xl relative'>
-          <img src="/img/hotimg.svg" alt="" />
+          <div className='bg-[#E7FAFE] w-[620px] h-[640px]'>
+            <div>
+              <div className='ml-5'>
+                <div className='flex items-center gap-4 bg-white rounded-full w-44 text-center h-8 mt-16 ml-5 justify-center'>
+                  <img src="./img/iconuse.svg" alt="" />
+                  <p className='text-black font-semibold'>Hot Recipes</p>
+                </div>
+                <h1 className='text-black text-[64px] font-semibold'>Spicy delicious chicken wings</h1>
+                <p className='text-black text-base font-normal'>Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt
+                  ut labore et dolore magna aliqut enim ad minim </p>
+              </div>
+            </div>
+            <div>
+            </div>
+          </div>
           <img src="/img/foodimg.svg" alt="" />
         </div>
-        <div className='absolute left-[670px] top-[100px]'>
+        <div className='absolute left-[670px] top-[120px]'>
           <img src="/img/bagimg.svg" alt="" />
-
         </div>
+      </div>
+      <div >
       </div>
     </>
   )
